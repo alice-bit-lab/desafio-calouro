db96536591aa6fe0800369a37e888a03120c1dbe
alice-bit-lab <alice2004cruz@gmail.com>
2026-06-27 22:13:49 -0300

    up

--- login.js
+++ login.js
@@ -1,10 +1,17 @@
 const express = require("express");
 const loginroutas = require("./routes/login");
+const { error } = require("node:console");
 const app = express();
+app.use(express.json());
 
 app.post('',(req,res) =>{
-
-})
+    const{nome,email,senha} = req.body;
+    if(!nome,!email,!senha){
+       return res.estatus(400).json({
+        error: "todos os campos tem que estar preenchidos !"
+       });
+    }
+});
 
 app.post('',(req,res) =>{
 
