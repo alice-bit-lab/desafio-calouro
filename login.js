e240c3460776fed5e8715544f03ad69487b86704
alice-bit-lab <alice2004cruz@gmail.com>
2026-06-29 16:11:27 -0300

    Update login.js

--- login.js
+++ login.js
@@ -1,27 +1,35 @@
 const express = require("express");
-const loginroutas = require("./routes/login");
+const userRoutas = require("./routes/login");
 const { error } = require("node:console");
 const app = express();
+
 app.use(express.json());
 
 const usuarios = [];
 
-app.post('',(req,res) =>{
+app.post('/',(req,res) =>{
     const{nome,email,senha} = req.body;
     if(!nome || !email || !senha){
        return res.estatus(400).json({
         error: "todos os campos tem que estar preenchidos !"
        });
     }
-    if(){}
-    
+    const umemaul = usuarios.find(iemail => iemail === email);
+    if(!umemaul){
+         res.send("emil já registrado");
+    }
     res.status(201).json({mensagem: "usuario criado com sucesso"});
+    
 });
+
+app.get('/',(req,res)=> {
+    res.render('index')
+})
 
 app.post('',(req,res) =>{
     const{email,senha} = req.body;
     if(!email || !senha){
-        return req.estatus(400).json({
+        return res.estatus(400).json({
             error : "todos os campos tem que estar prechidos !"
         })
     }
